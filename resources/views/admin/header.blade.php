e1586773c80ac38f6b6537d405cba1030b93d730
wenhaijun <763845314@qq.com>
2018-01-08 10:29:39 +0800

    2.0

--- resources/views/admin/header.blade.php
+++ resources/views/admin/header.blade.php
@@ -1,27 +1,7 @@
 <!doctype html>
 <html>
 
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Amaze UI Admin index Examples</title>
-    <meta name="description" content="这是一个 index 页面">
-    <meta name="keywords" content="index">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="renderer" content="webkit">
-    <meta http-equiv="Cache-Control" content="no-siteapp" />
-    <link rel="icon" type="image/png" href="/admin/i/favicon.png">
-    <link rel="apple-touch-icon-precomposed" href="/admin/i/app-icon72x72@2x.png">
-    <meta name="apple-mobile-web-app-title" content="Amaze UI" />
-    <link rel="stylesheet" href="/admin/css/amazeui.min.css" />
-    <link rel="stylesheet" href="/admin/css/admin.css">
-    <link rel="stylesheet" href="/admin/css/app.css">
-    <script src="/admin/js/echarts.min.js"></script>
-    <script src="/admin/js/jquery.min.js"></script>
-    <script src="/admin/js/amazeui.min.js"></script>
-    <script src="/admin/js/app.js"></script>
-    <script src="/admin/js/iscroll.js"></script>
-</head>
+
 
 <body data-type="chart">
 
@@ -148,4 +128,3 @@
             </ul>
         </div>
     </header>
-
