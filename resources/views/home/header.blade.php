d2246f51d9023659fe5246609d9e34030fd83674
songchuang <847045883@qq.com>
2018-01-09 09:23:52 +0800

    update

--- resources/views/home/header.blade.php
+++ resources/views/home/header.blade.php
@@ -1,3 +1,4 @@
+
 <div class="nav">
     <div class="nav-1">
     </div>
@@ -7,7 +8,7 @@
     </div>
     <div class="phone">400-060-6046</div>
     <div class="login"><a href="Login.html" >登录</a></div>
-    <div class="nav-nav">
+    <div class="nav-nav" id="xiao">
         <a href="/" class="action">首页</a>
         <a href="/www/buy">我要买车</a>
         <a href="/www/sell">我要卖车</a>
