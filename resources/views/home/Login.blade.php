4255587df6016c7190f8bc913b09191afad1feae
chenghang1 <972263627@qq.com>
2018-01-09 11:11:26 +0800

    sss

--- resources/views/home/Login.blade.php
+++ resources/views/home/Login.blade.php
@@ -9,28 +9,53 @@
 
 	<body>
 		<div class="login">
-			<h2>用户登录</h2>
+	
 			<div class="login-top">
 				<h1>用户登录</h1>
 				<form>
-					<input type="text" value="用户名" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = '用户名';}">
-					<input type="password" value="password" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'password';}">
+					<input type="text" value="请输入手机号码" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = '请输入手机号码';}">
+					<input id= "text" type="text" value="请输入手机验证码" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = '请输入手机验证码';}">
+					<input type="submit" value="获取验证码">
 				</form>
 				<div class="forgot">
-					<a href="#">忘记密码</a>
-					<input type="submit" value="登陆">
+		
+					<input type="submit" value="登 录">
 				</div>
 			</div>
 			<div class="login-bottom">
-				<h3>新用户 &nbsp;<a href="/IndexRegister">注册</a></h3>
+				<!-- <h3>新用户 &nbsp;<a href="/IndexRegister">注册</a></h3> -->
 			</div>
 		</div>
 		<div class="copyright">
-			<p>瓜子二手车登陆 2017.10.25
+			<p>瓜子二手车登陆
 				<a target="_blank" href="#"></a>
 			</p>
 		</div>
-
 	</body>
-
 </html>
+<script>
+	
+
+</script>
+
+<!-- 
+<?php
+    // use Flc\Dysms\Client;
+    // use Flc\Dysms\Request\SendSms;
+
+    // $config = [
+    //     'accessKeyId'    => 'LTAIJ33Rm3OWNv7b',
+    //     'accessKeySecret' => 'ViXzSAUkYRLQC73lCvyVT6waz8FzbS',
+    // ];
+
+    // $client  = new Client($config);
+    // $sendSms = new SendSms;
+    // $sendSms->setPhoneNumbers('17610330283');
+    // $sendSms->setSignName('飞天科技');
+    // $sendSms->setTemplateCode('SMS_120365798');
+    // $sendSms->setTemplateParam(['code' => rand(100000, 999999)]);
+    // $sendSms->setOutId('demo');
+
+    // print_r($client->execute($sendSms));
+
+ ?> -->
